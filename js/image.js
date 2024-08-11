497679178873880aca89c1388c3168e1b3d60df2
whes1015 <whes1015@gmail.com>
2024-08-11 16:16:30 +0800

    fix: url

--- js/image.js
+++ js/image.js
@@ -1,36 +1,26 @@
 load();
 
 function load() {
-  axios.get(`https://api-1.exptech.dev/file/images/eew/json/`).then((res) => {
-    document.getElementById(
-      "eew"
-    ).src = `https://api-1.exptech.dev/file/images/eew/${res.data.at(-1).name}`;
-  });
-  axios
-    .get(`https://api-1.exptech.dev/file/images/report/json/`)
-    .then((res) => {
-      document.getElementById(
-        "report"
-      ).src = `https://api-1.exptech.dev/file/images/report/${
-        res.data.at(-1).name
-      }`;
-    });
-  axios.get(`https://api-1.exptech.dev/file/images/lpgm/json/`).then((res) => {
-    document.getElementById(
-      "lpgm"
-    ).src = `https://api-1.exptech.dev/file/images/lpgm/${
+  axios.get(`https://${URL_MAIN}/file/images/eew/json/`).then((res) => {
+    document.getElementById("eew").src = `https://${URL_MAIN}/file/images/eew/${
       res.data.at(-1).name
     }`;
   });
-  axios
-    .get(`https://api-1.exptech.dev/file/images/intensity/json/`)
-    .then((res) => {
-      document.getElementById(
-        "intensity"
-      ).src = `https://api-1.exptech.dev/file/images/intensity/${
-        res.data.at(-1).name
-      }`;
-    });
+  axios.get(`https://${URL_MAIN}/file/images/report/json/`).then((res) => {
+    document.getElementById(
+      "report"
+    ).src = `https://${URL_MAIN}/file/images/report/${res.data.at(-1).name}`;
+  });
+  axios.get(`https://${URL_MAIN}/file/images/lpgm/json/`).then((res) => {
+    document.getElementById(
+      "lpgm"
+    ).src = `https://${URL_MAIN}/file/images/lpgm/${res.data.at(-1).name}`;
+  });
+  axios.get(`https://${URL_MAIN}/file/images/intensity/json/`).then((res) => {
+    document.getElementById(
+      "intensity"
+    ).src = `https://${URL_MAIN}/file/images/intensity/${res.data.at(-1).name}`;
+  });
 }
 
 setInterval(() => load(), 60_000);
